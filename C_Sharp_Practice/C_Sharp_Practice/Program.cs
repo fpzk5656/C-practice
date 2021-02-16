a107c7314d81824562685f598424469738c10990
tokaji <fpzk5656@gmail.com>
2021-02-17 00:02:54 +0900

    C# 기본 오브젝트 박싱 언박싱

---
+++ C_Sharp_Practice/C_Sharp_Practice/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string a = "안녕하세요?";
+            string b = "선을로 입니다";
+            bool c = true;
+            object d = a + b + c;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine();
+            Console.WriteLine(d);
+            int e;
+            e = (int)d;
+            Console.WriteLine(e);
+        }
+    }
+}
