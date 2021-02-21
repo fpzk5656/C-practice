a1c467ef917d938977bbe93a81fb006f102efea6
tokaji <fpzk5656@gmail.com>
2021-02-21 17:28:37 +0900

    .NET 공용 형식 시스템 (Common Type System 이하 CTS)이란. 모두가 사용하는 데이터 형식 체계를 뜻하고, '모두'란 C#을 비롯한, .NET 프레임워크를 지원하는 모든 언어를 뜻함. 즉, 공용 형식 시스템은 .NET 언어들이 반드시 따라야 하는 데이터 형식 표준임.

--- C_Sharp_Practice/C_Sharp_Practice/Program.cs
+++ C_Sharp_Practice/C_Sharp_Practice/Program.cs
@@ -11,27 +11,19 @@ namespace C_Sharp_Practice
         enum DialogResult { YES = 10, NO, CANCEL = 62, CONFIRM, OK = 99 }
         static void Main(string[] args)
         {
+            //.NET 공용 형식 시스템 (Common Type System 이하 CTS) C#에서 활용 예시]
 
-            //C#은 강력한 형식언어라서 깐깐하게 검사를 하는 언어 이지만,
-            // 약한 형식을 제공하기도 한다 var 이라는 키워드를 통해!
-            //var은 선언과 동시에 반드시 초기화 시켜줘야 하며,
-            //지역변수로만 사용가능
-            //..애초에 C#은 전역 변수를 지원하지 않도록 설계 되어 있다 함
+            System.Int32 a = 123;
+            int b = 456;
 
-            var a = 201;
-            var b = 27.826;
-            var c = "Hello";
-            var d = new int[] { 1, 7, 9 };
-            //프로그래머가 일일이 형식을 지정하는게 아닌,
-            //컴파일러가 알아서 지정해주는 키워드
+            Console.WriteLine("a type: {0}. value: {1}", a.GetType().ToString(), a);
+            Console.WriteLine("b type: {0}. value: {1}", b.GetType().ToString(), b);
 
-            Console.WriteLine("Type: {0}, Value: {1}", a.GetType(), a);
-            Console.WriteLine("Type: {0}, Value: {1}", b.GetType(), b);
-            Console.WriteLine("Type: {0}, Value: {1}", c.GetType(), c);
-            foreach (var e in d)
-                Console.Write("{0} ", e);
+            System.String c = "abc";
+            string d = "def";
 
-            Console.WriteLine();
+            Console.WriteLine("c type: {0}. value: {1}", c.GetType().ToString(), c);
+            Console.WriteLine("d type: {0}. value: {1}", d.GetType().ToString(), d);
 
         }
     }
