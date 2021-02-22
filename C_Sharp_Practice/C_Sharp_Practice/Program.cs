76dae14b4d8bcca805ce6e82eb4b182250c57b93
tokaji <fpzk5656@gmail.com>
2021-02-23 03:07:14 +0900

    bool값으로 쓸 수 있는 관계 연산자 방식와 조건에 따라 bool값을 결정 짓는 조건 연산자

--- C_Sharp_Practice/C_Sharp_Practice/Program.cs
+++ C_Sharp_Practice/C_Sharp_Practice/Program.cs
@@ -8,23 +8,22 @@ namespace C_Sharp_Practice
 {
     class Program
     {
-        enum DialogResult { YES = 10, NO, CANCEL = 62, CONFIRM, OK = 99 }
         static void Main(string[] args)
         {
-            //.NET 공용 형식 시스템 (Common Type System 이하 CTS) C#에서 활용 예시]
+            // 관계 연산자
 
-            System.Int32 a = 123;
-            int b = 456;
+            Console.WriteLine("3 > 4 : {0}", 3 > 4);
+            Console.WriteLine("3 >= 4 : {0}", 3 >= 4);
+            Console.WriteLine("3 < 4 : {0}", 3 < 4);
+            Console.WriteLine("3 <= 4 : {0}", 3 <= 4);
+            Console.WriteLine("3 == 4 : {0}", 3 == 4);
+            Console.WriteLine("3 != 4 : {0}", 3 != 4);
 
-            Console.WriteLine("a type: {0}. value: {1}", a.GetType().ToString(), a);
-            Console.WriteLine("b type: {0}. value: {1}", b.GetType().ToString(), b);
+            // 조건 연산자
+            //피연산자가 3개 이상
 
-            System.String c = "abc";
-            string d = "def";
-
-            Console.WriteLine("c type: {0}. value: {1}", c.GetType().ToString(), c);
-            Console.WriteLine("d type: {0}. value: {1}", d.GetType().ToString(), d);
-
+            string result = (10 % 2) == 0 ? "짝수" : "홀수";
+            Console.WriteLine(result);
         }
     }
 }
