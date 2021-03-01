aea653caaec35d6dd5c61b139e21256375624bf6
tokaji <fpzk5656@gmail.com>
2021-03-01 18:03:22 +0900

    Call by reference

--- C_Sharp_Practice/C_Sharp_Practice/Program.cs
+++ C_Sharp_Practice/C_Sharp_Practice/Program.cs
@@ -8,43 +8,28 @@ namespace C_Sharp_Practice
 {
     class Program
     {
+        // ref 키워드 사용법 예시
+        // 참조에 의한 전달 "Call by reference"로 넘길 수 있는 키워드이다.
+
+        //그냥 int 매개변수에 담아서 스왑하는것만으로는 "Call by value"이기 때문에 바로 전달이 되지 않으니,
+        //ref를 사용해 바로 매개변수에 복사해주는 변수에 영향을 끼칠 수 있다.
+        static void Swap(ref int a ,ref int b)
+        {
+            int temp = b;
+            b = a;
+            a = temp;
+        }
+        
         static void Main(string[] args)
         {
-            //텔레포트 하듯 구문을 뛰어넘어주는 점프문
-            // goto 문
+            int x = 3;
+            int y = 4;
 
-            Console.Write("종료 조건 (숫자)을 입력하세요 : ");
+            Console.WriteLine("x:{0}, y:{1}", x, y);
 
-            String input = Console.ReadLine();
+            Swap(ref x, ref y);
 
-            int input_number = Convert.ToInt32(input);
-            int exit_number = 0;
-
-            // 3중 for문으로 exit_number에 ++ 해주므로
-            // 2 *2*3* = 12가 된다. 그안에 입력한 숫자(input_number)와 같지 못하면
-            // EXIT_FOR를 만나지 않고 EXIT_PROGRAM만 만난다
-            for(int i = 0; i < 2; i++)
-            {
-                for(int j =0; j< 2; j++)
-                {
-                    for(int k = 0; k <3; k++)
-                    {
-                        if (exit_number++ == input_number)
-                            goto EXIT_FOR;
-
-                        Console.WriteLine(exit_number);
-                    }
-                }
-            }
-
-            goto EXIT_PROGRAM;
-
-        EXIT_FOR:
-            Console.WriteLine("\nExit nested for...");
-
-        EXIT_PROGRAM:
-            Console.WriteLine("Exit program...");
-
+            Console.WriteLine("x:{0}, y:{1}", x, y);
         }
     }
 }
