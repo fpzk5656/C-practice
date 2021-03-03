f9cbbd1de09db1b6c8f6faf155bd84d77dec8d5c
tokaji <fpzk5656@gmail.com>
2021-03-04 00:54:04 +0900

    재귀호출 - 피보나치

--- C_Sharp_Practice/C_Sharp_Practice/Program.cs
+++ C_Sharp_Practice/C_Sharp_Practice/Program.cs
@@ -8,28 +8,33 @@ namespace C_Sharp_Practice
 {
     class Program
     {
-        // ref 키워드 사용법 예시
-        // 참조에 의한 전달 "Call by reference"로 넘길 수 있는 키워드이다.
-
-        //그냥 int 매개변수에 담아서 스왑하는것만으로는 "Call by value"이기 때문에 바로 전달이 되지 않으니,
-        //ref를 사용해 바로 매개변수에 복사해주는 변수에 영향을 끼칠 수 있다.
-        static void Swap(ref int a ,ref int b)
+        static int Fibonacci(int n)
         {
-            int temp = b;
-            b = a;
-            a = temp;
+            if (n < 2)
+                return n;
+            else
+                return Fibonacci(n - 1) + Fibonacci(n - 2);
         }
-        
+
+        static void PrintProfile(string name, string phone)
+        {
+            if (name == "")
+            {
+                Console.WriteLine("이름을 입력해주세요.");
+                return;
+            }
+
+            Console.WriteLine("Name : {0}, Phone : {1}", name, phone);
+        }
+
         static void Main(string[] args)
         {
-            int x = 3;
-            int y = 4;
+            //재귀호출 예시
 
-            Console.WriteLine("x:{0}, y:{1}", x, y);
+            Console.WriteLine("10번째 피보나치 수 : {0}", Fibonacci(10));
 
-            Swap(ref x, ref y);
-
-            Console.WriteLine("x:{0}, y:{1}", x, y);
+            PrintProfile("", "123-4567");
+            PrintProfile("이창규", "456-1230");
         }
     }
 }
