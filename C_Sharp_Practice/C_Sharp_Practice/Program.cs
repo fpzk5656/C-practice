a51d317218b4a7f1a8c53e9c3574c0d1d75d9d60
tokaji <fpzk5656@gmail.com>
2021-03-07 19:18:51 +0900

    오버로딩

--- C_Sharp_Practice/C_Sharp_Practice/Program.cs
+++ C_Sharp_Practice/C_Sharp_Practice/Program.cs
@@ -8,24 +8,37 @@ namespace C_Sharp_Practice
 {
     class Program
     {
-        static void Divide(int a, int b, out int quotient, out int remainder)
+        //오버로딩(Overloading)
+        static int Plus(int a, int b)
         {
-            //out은 ref랑 비슷하지만 참조한 매개변수에 영향을 주지 않으면 에러가 뜬다.
+            Console.WriteLine("Calling int Plus(int,int)...");
+            return a + b;
+        }
 
-            quotient = a / b;
-            remainder = a % b;
+        static int Plus(int a, int b, int c)
+        {
+            Console.WriteLine("Calling int Plus(int,int,int)...");
+            return a + b + c;
+        }
+
+        static double Plus(double a, double b)
+        {
+            Console.WriteLine("Calling double Plus(double,double)...");
+            return a + b;
+        }
+
+        static double Plus(int a, double b)
+        {
+            Console.WriteLine("Calling double Plus(int,double)...");
+            return a + b;
         }
 
         static void Main(string[] args)
         {
-            int a = 20;
-            int b = 3;
-            int c;
-            int d;
-
-            Divide(a, b, out c, out d);
-
-            Console.WriteLine("a:{0}, b:{1}, a/b:{2}, a%b:{3}", a, b, c, d);
+            Console.WriteLine(Plus(1, 2));
+            Console.WriteLine(Plus(1, 2, 3));
+            Console.WriteLine(Plus(1.0, 2.4));
+            Console.WriteLine(Plus(1, 2.4));
         }
     }
 }
